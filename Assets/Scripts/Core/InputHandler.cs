4cb56db5f663c19e61635af12fcbf245963d742e
Bo Seung Park <pbs04054@gmail.com>
2020-07-18 22:32:28 +0900

    Simple Player Control

--- Assets/Scripts/Core/InputHandler.cs
+++ Assets/Scripts/Core/InputHandler.cs
@@ -16,7 +16,16 @@ public struct UserKeySetting
 public class InputHandler : SingletonBehaviour<InputHandler>
 {
     private UserKeySetting setting;
-    public Action OnUpKeyDown;
+
+    public Action<Vector2> OnUpKey;
+    public Action<Vector2> OnDownKey;
+    public Action<Vector2> OnLeftKey;
+    public Action<Vector2> OnRightKey;
+
+    public Action<Vector2> OnUpKeyDown;
+    public Action<Vector2> OnDownKeyDown;
+    public Action<Vector2> OnLeftKeyDown;
+    public Action<Vector2> OnRightKeyDown;
 
     public Action<Vector2> OnAttackKeyDown;
 
@@ -24,9 +33,37 @@ public class InputHandler : SingletonBehaviour<InputHandler>
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            OnUpKey?.Invoke(Vector2.up);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            OnDownKey?.Invoke(Vector2.down);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            OnLeftKey?.Invoke(Vector2.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            OnRightKey?.Invoke(Vector2.right);
+        }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            OnUpKeyDown?.Invoke();
+            OnUpKeyDown?.Invoke(Vector2.up);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            OnDownKeyDown?.Invoke(Vector2.down);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnLeftKeyDown?.Invoke(Vector2.left);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            OnRightKeyDown?.Invoke(Vector2.right);
         }
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        InputHandler.inst.OnLeftKey += Move;
+        InputHandler.inst.OnRightKey += Move;
+    }
+
+    private void OnDisable()
+    {
+        InputHandler.inst.OnLeftKey -= Move;
+        InputHandler.inst.OnRightKey -= Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        Vector3 moveDirection = direction;
+        transform.position += moveDirection * 2 * Time.deltaTime;
+    }
+}
