fb97ed2944de2fa3ddcc020cf445934bace23be5
jasson31 <jasson31@naver.com>
2020-07-22 19:54:48 +0900

    Made two UI sceens and scripts

---
+++ Assets/Scripts/MainUIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIManager : UIManager
+{
+    protected override void OnMount()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnUnmount()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
