cabb3d1d78795660ec218301a23f6ae5cf07a8f2
Hyunjin <hyunjin1109@korea.ac.kr>
2020-07-25 03:52:19 +0900

    Modify tutorial UI Add bosses UI

--- Assets/Scripts/SelectUIManager.cs
+++ Assets/Scripts/SelectUIManager.cs
@@ -1,14 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SelectUIManager : UIManager
 {
-
-    public void t()
+    public bool tutorial = false;
+    public Button B0, B1, B2, B3;
+ 
+    public void t1()
     {
         GameObject.Find("T1").SetActive(false);
+        B0.gameObject.SetActive(true);
+        
+}
+    public void t2()
+    {
+        GameObject.Find("T1").SetActive(false);
+        tutorial = true;
+        B0.GetComponentInChildren<Text>().text = "클리어";
+        b0();
+        
+}
+
+    public void b0()
+    {
+        Button[] arr = { B0, B1, B2, B3 };
+        foreach(Button a in arr)
+        {
+            a.gameObject.SetActive(true);
+        }
     }
+
+    
     protected override void OnMount()
     {
         throw new System.NotImplementedException();
@@ -22,11 +47,14 @@ public class SelectUIManager : UIManager
     // Start is called before the first frame update
     void Start()
     {
-        
+        B0.gameObject.SetActive(false);
+        B1.gameObject.SetActive(false);
+        B2.gameObject.SetActive(false);
+        B3.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
