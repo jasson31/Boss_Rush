bd6ad87e59fdfb134de30cad118767213a50e5e9
jasson31 <jasson31@naver.com>
2020-08-06 03:31:07 +0900

    거미 보스 패턴 1 구현 중

--- Assets/Scripts/Boss/SpiderBoss.cs
+++ Assets/Scripts/Boss/SpiderBoss.cs
@@ -2,17 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiderBoss : MonoBehaviour
+public class SpiderBoss : Boss
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Bounds map;
+
+    [SerializeField]
+    private GameObject webBullet;
+
+
+
+    private void OnDrawGizmosSelected()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(map.center, map.size);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override Queue<IEnumerator> DecideNextRoutine()
     {
-        
+        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>();
+        float rand = Random.value;
+
+
+        switch (Phase)
+        {
+            case 0:
+                nextRoutines.Enqueue(NewActionRoutine(WebShootRoutine()));
+                nextRoutines.Enqueue(NewActionRoutine(WaitRoutine(2)));
+                if (rand < 0.2f)
+                {
+                }
+                break;
+        }
+
+
+        return nextRoutines;
+    }
+
+
+    private IEnumerator WebShootRoutine()
+    {
+        float webBulletSpeed = 10;
+        Vector3 dir = (Game.inst.player.position - transform.position).normalized;
+        GameObject newWebBullet = Instantiate(webBullet, transform.position, Quaternion.identity);
+        newWebBullet.GetComponent<Rigidbody2D>().velocity = dir * webBulletSpeed;
+        newWebBullet.AddComponent<WebBullet>();
+        Destroy(newWebBullet, 5);
+        yield return null;
+    }
+
+
+    protected override void OnStunned()
+    {
+
+    }
+}
+
+class WebBullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            Destroy(gameObject, 0.1f);
+        }
     }
 }
