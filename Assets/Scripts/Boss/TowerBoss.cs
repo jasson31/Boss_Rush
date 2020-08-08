4eb0d4443b6138ce4ab8ab73b04de827406a40a6
Hyunjin <hyunjin1109@korea.ac.kr>
2020-08-08 19:09:17 +0900

    Add BossTower Phase2 Routines1 2a 2b

--- Assets/Scripts/Boss/TowerBoss.cs
+++ Assets/Scripts/Boss/TowerBoss.cs
@@ -8,8 +8,17 @@ public class TowerBoss : Boss
     private GameObject normalBullet;
     [SerializeField]
     private Vector3 shootPos;
+    [SerializeField]
+    private GameObject laserCollider;
+    [SerializeField]
+    private Bounds map;
+    [SerializeField]
+    private LineRenderer lr, lr2;
+    const float laserReadyWidth = 0.1f;
+    const float laserShootWidth = 2.0f;
 
     const float bulletSpeed = 5;
+
 
 
 
@@ -20,9 +29,10 @@ public class TowerBoss : Boss
 
         float rand = Random.value;
 
-        switch (0)
+        switch (1)
         {
             case 0:
+
                 if (rand < 0.33f)
                 {
 
@@ -46,6 +56,29 @@ public class TowerBoss : Boss
                 break;
 
             case 1:
+
+                Vector3 distance = shootPos - GetPlayerPos();
+
+                if(rand < 0.5f)
+                {
+                    nextRoutines.Enqueue(NewActionRoutine(LaserRoutine(1.0f)));
+                    nextRoutines.Enqueue(NewActionRoutine(WaitRoutine(1.0f)));
+                }
+                else
+                {
+                    if (distance.magnitude > 9)
+                    {
+                        nextRoutines.Enqueue(NewActionRoutine(LaserRoutine2(1.0f)));
+                        nextRoutines.Enqueue(NewActionRoutine(WaitRoutine(1.0f)));
+                    }
+                    else
+                    {
+                        nextRoutines.Enqueue(NewActionRoutine(LaserRoutine2b(1.0f)));
+                        nextRoutines.Enqueue(NewActionRoutine(WaitRoutine(1.0f)));
+                    }
+                }
+                
+                
                 break;
         }
 
@@ -61,7 +94,7 @@ public class TowerBoss : Boss
             {
                 Instantiate(normalBullet, shootPos + new Vector3(Mathf.Cos(j * 30 * Mathf.PI / 180), Mathf.Sin(j * 30 * Mathf.PI / 180), 0), Quaternion.identity).GetComponent<Rigidbody2D>().velocity
                     = (new Vector3(Mathf.Cos(j * 30 * Mathf.PI / 180), Mathf.Sin(j * 30 * Mathf.PI / 180), 0)).normalized * bulletSpeed;
-                yield return null;
+                //yield return null;
             }
             yield return new WaitForSeconds(interval);
 
@@ -92,6 +125,158 @@ public class TowerBoss : Boss
         yield return new WaitForSeconds(interval);
         }
 
+    }
+
+    private IEnumerator LaserRoutine(float waitTime)
+    {
+        lr.enabled = true;
+
+        Vector3 lineEndPos = GetPlayerPos();
+
+        lr.SetPosition(0, shootPos);
+        lr.SetPosition(1, lineEndPos);
+
+        for (float t = 0; t < waitTime; t += Time.deltaTime)
+        {
+
+            lineEndPos = GetPlayerPos();
+            lr.SetPosition(0, shootPos);
+            lr.SetPosition(1, lineEndPos);
+            yield return null;
+        }
+
+
+        Vector3 diff = lineEndPos - shootPos;
+
+
+        lr.startWidth = laserShootWidth;
+        lr.endWidth = laserShootWidth;
+
+        yield return new WaitForSeconds(0.5f);
+
+        for (float t = 0; t < 0.2f; t += Time.deltaTime)
+        {
+            float size = Mathf.Lerp(laserShootWidth, laserReadyWidth, t / 0.2f);
+            lr.startWidth = size;
+            lr.endWidth = size;
+            yield return null;
+        }
+
+        lr.startWidth = laserReadyWidth;
+        lr.endWidth = laserReadyWidth;
+        lr.enabled = false;
+    }
+    
+    private IEnumerator LaserRoutine2(float waitTime)
+    {
+        lr.enabled = true;
+        lr2.enabled = true;
+
+        Vector3 end1 = new Vector3(map.min.x - 1, map.min.y, 0);
+        Vector3 end2 = new Vector3(map.max.x + 1, map.min.y, 0);
+
+
+        lr.SetPosition(0, shootPos);
+        lr.SetPosition(1, end1);
+
+        lr2.SetPosition(0, shootPos);
+        lr2.SetPosition(1, end2);
+
+        yield return new WaitForSeconds(waitTime);
+
+
+        Vector3 fin = new Vector3(0, map.min.y, 0);
+
+        lr.startWidth = laserShootWidth;
+        lr.endWidth = laserShootWidth;
+        
+        lr2.startWidth = laserShootWidth;
+        lr2.endWidth = laserShootWidth;
+
+        while (end1.x < 0)
+        {
+            end1 = end1 + new Vector3(0.1f, 0, 0);
+            end2 = end2 - new Vector3(0.1f, 0, 0);
+            lr.SetPosition(1, end1);
+            lr2.SetPosition(1, end2);
+
+            yield return new WaitForSeconds(0.015f);
+        }
+
+
+        for (float t = 0; t < 0.2f; t += Time.deltaTime)
+        {
+            float size = Mathf.Lerp(laserShootWidth, laserReadyWidth, t / 0.2f);
+            lr.startWidth = size;
+            lr.endWidth = size;
+            
+            lr2.startWidth = size;
+            lr2.endWidth = size;
+            yield return null;
+        }
+
+        lr.startWidth = laserReadyWidth;
+        lr.endWidth = laserReadyWidth;
+        lr.enabled = false;
+        
+        lr2.startWidth = laserReadyWidth;
+        lr2.endWidth = laserReadyWidth;
+        lr2.enabled = false;
+    }
+
+    private IEnumerator LaserRoutine2b(float waitTime)
+    {
+        lr.enabled = true;
+        lr2.enabled = true;
+
+        Vector3 start = new Vector3(0, map.min.y, 0);
+
+        lr.SetPosition(0, shootPos);
+        lr.SetPosition(1, start);
+
+        lr2.SetPosition(0, shootPos);
+        lr2.SetPosition(1, start);
+
+        yield return new WaitForSeconds(waitTime);
+
+        Vector3 end1 = new Vector3(0, map.min.y, 0);
+        Vector3 end2 = new Vector3(0, map.min.y, 0);
+
+        lr.startWidth = laserShootWidth;
+        lr.endWidth = laserShootWidth;
+
+        lr2.startWidth = laserShootWidth;
+        lr2.endWidth = laserShootWidth;
+
+        while (end1.x > map.min.x - 1)
+        {
+            end1 = end1 - new Vector3(0.1f, 0, 0);
+            end2 = end2 + new Vector3(0.1f, 0, 0);
+            lr.SetPosition(1, end1);
+            lr2.SetPosition(1, end2);
+
+            yield return new WaitForSeconds(0.015f);
+        }
+
+
+        for (float t = 0; t < 0.2f; t += Time.deltaTime)
+        {
+            float size = Mathf.Lerp(laserShootWidth, laserReadyWidth, t / 0.2f);
+            lr.startWidth = size;
+            lr.endWidth = size;
+
+            lr2.startWidth = size;
+            lr2.endWidth = size;
+            yield return null;
+        }
+
+        lr.startWidth = laserReadyWidth;
+        lr.endWidth = laserReadyWidth;
+        lr.enabled = false;
+
+        lr2.startWidth = laserReadyWidth;
+        lr2.endWidth = laserReadyWidth;
+        lr2.enabled = false;
     }
 
 
