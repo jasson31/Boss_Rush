93726ff7946adf9a5250c76f6908a343de1235fb
Bo Seung Park <pbs04054@gmail.com>
2020-08-24 01:53:30 +0900

    무기 에디터 초안

---
+++ Assets/Scripts/General/JsonParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonParser<T>
+{
+	public static string ToJson(T obj)
+	{
+		return JsonUtility.ToJson(obj);
+	}
+
+	public static T FromJson(string json)
+	{
+		T obj = default(T);
+		try
+		{
+			obj = JsonUtility.FromJson<T>(json);
+		}
+		catch
+		{
+			Debug.LogError("Invalid json format!");
+			return obj;
+		}
+		return obj;
+	}
+}
