cf285675f510dd3e2ac42274122365aeabf8f631
jomin1418 <joming1418@gmail.com>
2020-08-28 14:37:29 +0900

    기본 틀

--- Assets/Scripts/Boss/VeteranBoss/VeteranBoss.cs
+++ Assets/Scripts/Boss/VeteranBoss/VeteranBoss.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class VeteranBoss : Boss
 {
-    
+    protected override Queue<IEnumerator> DecideNextRoutine()
+    {
+        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>;
+        float rand = Random.value;
+
+        switch (Phase)
+        {
+            case 0:
+                if(rand < 0.2f)
+                {
+                    nextRoutines.Enqueue(NewActionRoutine());
+                }
+                else if(rand < 0.4f)
+                {
+                    nextRoutines.Enqueue(NewActionRoutine());
+                }
+                else if(rand < 0.6f)
+                {
+                    nextRoutines.Enqueue(NewActionRoutine());
+                }
+                else if(rand < 0.8f)
+                {
+                    nextRoutines.Enqueue(NewActionRoutine());
+                }
+                else if(rand < 0.9f)
+                {
+                    nextRoutines.Enqueue(NewActionRoutine());
+                }
+                else
+                {
+                    nextRoutines.Enqueue(NewActionRoutine());
+                }
+
+        }
+
+        return nextRoutines;
+    }
 }
