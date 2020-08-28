56f1a08bbda7f68a8f806b172949b2bef16d04f4
jomin1418 <joming1418@gmail.com>
2020-08-28 16:14:34 +0900

    기본틀 2

--- Assets/Scripts/Boss/VeteranBoss/VeteranBoss.cs
+++ Assets/Scripts/Boss/VeteranBoss/VeteranBoss.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class VeteranBoss : Boss
 {
+
+    [SerializeField]
+
+
+
     protected override Queue<IEnumerator> DecideNextRoutine()
     {
-        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>;
+        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>();
         float rand = Random.value;
 
         switch (Phase)
@@ -14,31 +19,58 @@ public class VeteranBoss : Boss
             case 0:
                 if(rand < 0.2f)
                 {
-                    nextRoutines.Enqueue(NewActionRoutine());
+                    nextRoutines.Enqueue(NewActionRoutine(AKMRoutine()));
                 }
                 else if(rand < 0.4f)
                 {
-                    nextRoutines.Enqueue(NewActionRoutine());
+                    nextRoutines.Enqueue(NewActionRoutine(SniperRoutine()));
                 }
                 else if(rand < 0.6f)
                 {
-                    nextRoutines.Enqueue(NewActionRoutine());
+                    nextRoutines.Enqueue(NewActionRoutine(BayonetRoutine()));
                 }
                 else if(rand < 0.8f)
                 {
-                    nextRoutines.Enqueue(NewActionRoutine());
+                    nextRoutines.Enqueue(NewActionRoutine(GrenadeRoutine()));
                 }
                 else if(rand < 0.9f)
                 {
-                    nextRoutines.Enqueue(NewActionRoutine());
+                    nextRoutines.Enqueue(NewActionRoutine(SmokeRoutine()));
                 }
                 else
                 {
-                    nextRoutines.Enqueue(NewActionRoutine());
+                    nextRoutines.Enqueue(NewActionRoutine(FlareRoutine()));
                 }
 
         }
 
         return nextRoutines;
+    }
+
+    private IEnumerator AKMRoutine()
+    {
+
+        yield return null;
+    }
+
+    private IEnumerator SniperRoutine()
+    {
+
+    }
+    
+    public override void GetDamaged(int damage)
+    {
+        base.GetDamaged(damage);
+        if (MaxHealth * 0.25f >= Health && Phase == 0)
+        {
+            Phase = 1;
+        }
+        if (Health <= 0)
+            gameObject.SetActive(false);
+    }
+
+    protected override OnStunned()
+    {
+
     }
 }
